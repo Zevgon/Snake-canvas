d071c374bc6388b12c8c138da2922cd04065f2f5
user.name = 'Yale Work <user.email = 'yale.work@gmail.com>
2017-05-28 18:35:18 -0700

    Split up game and board

--- game.js
+++ game.js
@@ -1,95 +1,30 @@
-import Snake from './snake.js';
-import {
-  COLOR_MAP,
-  KEY_TO_DIR,
-  VALID_DIRS,
-} from './constants';
+import Grid from './grid';
+import { KEY_TO_DIR } from './constants';
 
 export default class Game {
   constructor(rows, columns) {
     this.rows = rows;
     this.columns = columns;
-    const canvas = document.getElementById('canvas');
-    const width = parseInt($(canvas).css('width'));
-    const height = parseInt($(canvas).css('height'));
-    this.widthRatio = parseInt(width / columns);
-    this.heightRatio = parseInt(height / rows);
-    this.grid = canvas.getContext("2d");
-    this.grid.scale(this.widthRatio, this.heightRatio);
-    this.reset();
+    this.grid = new Grid(rows, columns, document.getElementById('canvas'));
     this.startEventListner();
-  }
-
-  reset() {
-    this.snake = new Snake([this.getStartingCoord()]);
-    this.fillSquare(this.snake.head(), 'snake');
-    this.direction = 'n';
-  }
-
-  validDirection(dir) {
-    return VALID_DIRS[this.direction].includes(dir);
   }
 
   startEventListner() {
     document.addEventListener('keydown', e => {
       const dir = KEY_TO_DIR[e.which];
-      if (this.validDirection(dir)) {
-        this.direction = dir;
-      }
+      this.grid.changeDirection(dir);
     });
-  }
-
-  getStartingCoord() {
-    const x = parseInt(this.columns / 2);
-    const y = this.rows - 1;
-    return [x, y];
-  }
-
-  coordToCanvas(coord) {
-    return [coord[0], coord[1], 1, 1];
   }
 
   start() {
-    this.interval = setInterval(this.move.bind(this), 200);
+    this.interval = setInterval(this.handleMove.bind(this), 200);
   }
 
-  fillSquare(coord, type) {
-    const color = COLOR_MAP[type];
-    if (!color) {
-      throw 'Invalid color!';
-    }
-    this.grid.fillStyle = color;
-    this.grid.fillRect(...this.coordToCanvas(coord));
-  }
-
-  validPos(pos) {
-    if (this.snake.set.has(pos.toString()) &&
-        pos.toString() !== this.snake.head().toString()) {
-      return false;
-    }
-    if (pos[0] < 0 || pos[0] >= this.columns || pos[1] < 0 || pos[1] >= this.rows) {
-      return false;
-    }
-    return true;
-  }
-
-  clearSnake() {
-    this.snake.queue.forEach(coord => {
-      this.fillSquare(coord, 'empty');
-    });
-  }
-
-  move() {
-    const tail = this.snake.move(this.direction);
-    if (tail) {
-      this.fillSquare(tail, 'empty');
-    }
-    if (this.validPos(this.snake.head())) {
-      this.fillSquare(this.snake.head(), 'snake');
-    } else {
+  handleMove() {
+    const moved = this.grid.move();
+    if (!moved) {
       clearInterval(this.interval);
-      this.clearSnake();
-      this.reset();
+      this.grid.reset();
     }
   }
 }
---
+++ grid.js
@@ -0,0 +1,89 @@
+import Snake from './snake';
+import {
+  VALID_DIRS,
+  COLOR_MAP,
+} from './constants';
+
+export default class Grid {
+  constructor(rows, cols, canvasEl) {
+    this.rows = rows;
+    this.cols = cols;
+    this.canvas = canvasEl.getContext('2d');
+    this.width = parseInt($(canvas).css('width'));
+    this.height = parseInt($(canvas).css('height'));
+    this.widthRatio = parseInt(this.width / cols);
+    this.heightRatio = parseInt(this.height / rows);
+    this.grid = canvas.getContext("2d");
+    this.grid.scale(this.widthRatio, this.heightRatio);
+    this.direction = 'n';
+    this.reset();
+  }
+
+  reset() {
+    this.snake = new Snake([this.getStartingCoord()]);
+    this.grid.clearRect(0, 0, this.width, this.height);
+    this.direction = 'n';
+    this.fillSquare(this.snake.head(), 'snake');
+  }
+
+  changeDirection(newDir) {
+    if (this.validDirection(this.direction, newDir)) {
+      this.direction = newDir;
+    }
+  }
+
+  fillSquare(coord, type) {
+    const color = COLOR_MAP[type];
+    if (!color) {
+      throw 'Invalid color!';
+    }
+    this.grid.fillStyle = color;
+    this.grid.fillRect(...this.coordToCanvas(coord));
+  }
+
+  validPos(pos) {
+    if (this.snake.set.has(pos.toString()) &&
+        pos.toString() !== this.snake.head().toString()) {
+      return false;
+    }
+    if (pos[0] < 0 || pos[0] >= this.columns || pos[1] < 0 || pos[1] >= this.rows) {
+      return false;
+    }
+    return true;
+  }
+
+  clearSnake() {
+    this.snake.queue.forEach(coord => {
+      this.fillSquare(coord, 'empty');
+    });
+  }
+
+  getStartingCoord() {
+    const x = parseInt(this.cols / 2);
+    const y = this.rows - 1;
+    return [x, y];
+  }
+
+  coordToCanvas(coord) {
+    return [coord[0], coord[1], 1, 1];
+  }
+
+  validDirection(oldDir, newDir) {
+    return VALID_DIRS[oldDir].includes(newDir);
+  }
+
+  move() {
+    const tail = this.snake.move(this.direction);
+    if (tail) {
+      this.fillSquare(tail, 'empty');
+    }
+    if (this.validPos(this.snake.head())) {
+      this.fillSquare(this.snake.head(), 'snake');
+      return true;
+    } else {
+      this.clearSnake();
+      this.reset();
+      return false;
+    }
+  }
+}
